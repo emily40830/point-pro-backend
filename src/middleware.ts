653146bb1ed127f4532fa7564512feb1cda40b27
黃士桓 <huan5678@gmail.com>
2023-05-20 22:31:09 +0800

    :construction: [wip]add admin order controller
    目前還缺少驗證登入者帶入token的部分

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request } from 'express';
 import { ApiResponse, AuthRequest } from './types/shared';
 import { object, string, number } from 'yup';
 import session from 'express-session';
+import { orderSchema } from './schemas';
 
 const verifySchema = object({
   sub: string().required(),
@@ -60,4 +61,16 @@ export const verifyMiddleware = (excludes?: string[]) => (req: AuthRequest, res:
 
 export const errorMiddleware = (error: Error, req: Request, res: ApiResponse, next: NextFunction) => {
   return res.status(500).send({ message: `${error.name} ${error.message}`, result: null });
+};
+
+export const validateOrderData = (req: Request, res: ApiResponse, next: NextFunction) => {
+  try {
+    const orderData = req.body;
+
+    orderSchema.validateSync(orderData);
+
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid order data', result: error?.toString() });
+  }
 };
