8e644749151fd24dd66ec491d13376b5e72441e2
emily40830 <emily40830@gmail.com>
2023-05-23 19:46:52 +0800

    feat: add verify mechnism

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,63 @@
+import jwt from 'jsonwebtoken';
 import { NextFunction, Request } from 'express';
-import { ApiResponse } from './types/shared';
+import { ApiResponse, AuthRequest } from './types/shared';
+import { object, string, number } from 'yup';
+import session from 'express-session';
 
-const errorMiddleware = (error: Error, req: Request, res: ApiResponse, next: NextFunction) => {
-  return res.status(500).send({ message: `${error.name} ${error.message}`, result: null });
+const verifySchema = object({
+  sub: string().required(),
+  iat: number().required(),
+  exp: number().required(),
+  memberId: string().required(),
+  account: string().required(),
+  email: string().optional(),
+  role: string().required(),
+});
+
+const secret = process.env.POINT_PRO_SECRET || 'point-proo';
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+export const sessionMiddleware = session({
+  secret: 'point-pro',
+  saveUninitialized: true,
+  cookie: { maxAge: oneDay },
+  resave: false,
+});
+
+export const verifyMiddleware = (excludes?: string[]) => (req: AuthRequest, res: ApiResponse, next: NextFunction) => {
+  console.log(req.path);
+  console.log(excludes);
+
+  if (excludes && excludes.includes(req.path)) {
+    return next();
+  }
+  const token = req.headers.authorization?.split(' ')[1];
+  //Authorization: 'Bearer TOKEN'
+  if (!token) {
+    res.status(401).send({
+      message: 'invalid token',
+      result: null,
+    });
+  } else {
+    const decoded = jwt.verify(token, secret);
+    try {
+      verifySchema.validateSync(decoded);
+      const user = verifySchema.cast(decoded);
+      console.log('user', user);
+
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(403).send({
+        message: error as string,
+        result: null,
+      });
+    }
+  }
 };
 
-export default errorMiddleware;
+export const errorMiddleware = (error: Error, req: Request, res: ApiResponse, next: NextFunction) => {
+  return res.status(500).send({ message: `${error.name} ${error.message}`, result: null });
+};
--- src/types/shared.d.ts
+++ src/types/shared.d.ts
@@ -1,4 +1,32 @@
-import { Response } from 'express';
+import { Response, Request } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        sub: string;
+        iat: number;
+        exp: number;
+        memberId: string;
+        account: string;
+        email?: string;
+        role: string;
+      };
+    }
+  }
+}
+
+type AuthRequest = Request & {
+  user?: {
+    sub: string;
+    iat: number;
+    exp: number;
+    memberId: string;
+    account: string;
+    email?: string;
+    role: string;
+  };
+};
 
 type ApiResponse = Response<{
   message: string;
