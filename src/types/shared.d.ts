69b6eeb1467bd6c4ff4c9d2f21d922b28c5f8849
emily40830 <emily40830@gmail.com>
2023-06-05 09:52:39 +0800

    feat: Add two type verify schema

--- src/types/shared.d.ts
+++ src/types/shared.d.ts
@@ -17,15 +17,25 @@ declare global {
 }
 
 type AuthRequest = Request & {
-  user?: {
-    sub: string;
-    iat: number;
-    exp: number;
-    memberId: string;
-    account: string;
-    email?: string;
-    role: string;
-  };
+  auth?:
+    | { role: string } & (
+        | {
+            sub: string;
+            iat: number;
+            exp: number;
+            memberId: string;
+            account: string;
+            email?: string;
+          }
+        | {
+            reservationLogId: string;
+            reservationType?: string;
+            startTime: Date;
+            seatNo: string;
+            periodStartTime?: Date;
+            periodEndTime?: Date;
+          }
+      );
 };
 
 type ApiResponse = Response<{
