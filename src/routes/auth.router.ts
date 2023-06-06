0a345188c654f18075647ca6ca6ff56bcee7b744
Alan Huang <alan.h@tc-gaming.com>
2023-06-06 17:04:22 +0800

    feat: add user-info api

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -7,5 +7,6 @@ authRouter.post('/register', AuthController.registerHandler);
 authRouter.post('/login', AuthController.loginHandler);
 authRouter.post('/logout', AuthController.logoutHandler);
 authRouter.get('/token', AuthController.generateTokenHandler);
+authRouter.get('/user-info', AuthController.decodeTokenHandler);
 
 export default authRouter;
