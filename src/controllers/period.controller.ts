cd138836804d9bbe2bd086d86cb4f718a3996be0
emily40830 <emily40830@gmail.com>
2023-06-19 02:01:07 +0800

    feat: update period controller

--- src/controllers/period.controller.ts
+++ src/controllers/period.controller.ts
@@ -2,8 +2,22 @@ import { object, date as dateSchema } from 'yup';
 import { prismaClient } from '../helpers';
 import { ApiResponse, AuthRequest } from '../types/shared';
 
+type PeriodInfo = {
+  id: string;
+  periodStartedAt: Date;
+  amount: number;
+  available: number;
+};
+
+type DatePeriodInfo = {
+  date: Date;
+  periods: PeriodInfo[];
+  totalAmount: number;
+  totalAvailable: number;
+};
+
 class PeriodController {
-  public static getPeriods = async (req: AuthRequest, res: ApiResponse) => {
+  public static getPeriodByDate = async (req: AuthRequest, res: ApiResponse) => {
     const querySchema = object({
       date: dateSchema()
         .optional()
@@ -38,7 +52,7 @@ class PeriodController {
         include: {
           seatPeriod: {
             where: {
-              canBooked: false,
+              canOnlineBooked: true,
             },
             include: {
               seat: {
@@ -61,6 +75,84 @@ class PeriodController {
         result: null,
       });
     }
+  };
+
+  public static getPeriods = async (req: AuthRequest, res: ApiResponse<DatePeriodInfo[]>) => {
+    const periods = await prismaClient.period.findMany({
+      where: {
+        startedAt: {
+          gte: new Date(),
+        },
+        seatPeriod: {
+          some: {
+            canOnlineBooked: true,
+          },
+        },
+      },
+      include: {
+        seatPeriod: {
+          where: {
+            canOnlineBooked: true,
+          },
+          include: {
+            seat: {
+              select: {
+                prefix: true,
+                no: true,
+                amount: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const periodsWithAmount: PeriodInfo[] = periods.map((period) => {
+      let total = 0;
+      let available = 0;
+
+      period.seatPeriod.forEach((seatPeriod) => {
+        if (seatPeriod.canBooked) {
+          available += seatPeriod.seat.amount;
+        }
+        total += seatPeriod.seat.amount;
+      });
+      return {
+        id: period.id,
+        periodStartedAt: period.startedAt,
+        amount: total,
+        available,
+      };
+    });
+
+    const datePeriodsWithAmount = periodsWithAmount.reduce<DatePeriodInfo[]>((prev, curr) => {
+      const targets = prev.filter((d) => d.date.toDateString() === curr.periodStartedAt.toDateString());
+      if (targets.length === 0) {
+        const newDatePeriod: DatePeriodInfo = {
+          date: new Date(curr.periodStartedAt.toDateString()),
+          periods: [curr],
+          totalAmount: curr.amount,
+          totalAvailable: curr.available,
+        };
+
+        return [...prev, newDatePeriod];
+      }
+      const target = targets[0];
+
+      const newTarget = {
+        ...target,
+        periods: [...target.periods, curr],
+        totalAmount: target.totalAmount + curr.amount,
+        totalAvailable: target.totalAvailable + curr.available,
+      };
+
+      return [...prev.filter((d) => d.date.toDateString() !== curr.periodStartedAt.toDateString()), newTarget];
+    }, []);
+
+    res.status(200).send({
+      message: 'Successfully get periods',
+      result: datePeriodsWithAmount,
+    });
   };
 }
 
